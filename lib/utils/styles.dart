5dd3446b9bb27e29cd1dc5d91ad34d0aa032d44a
= <=>
2020-11-21 18:58:05 -0600

    Se agrego la clase SettingsStyle y se organizaron mejor los estilos por ejemplo abiendo un solo textStyle

--- lib/utils/styles.dart
+++ lib/utils/styles.dart
@@ -3,18 +3,17 @@ import 'package:helpers/helpers.dart';
 
 class VideoViewerStyle {
   VideoViewerStyle({
-    VideoVolumeBarStyle volumeBarStyle,
     VideoProgressBarStyle progressBarStyle,
-    ForwardAndRewindStyle forwardAndRewindStyle,
     PlayAndPauseWidgetStyle playAndPauseStyle,
+    SettingsStyle settingsStyle,
+    ForwardAndRewindStyle forwardAndRewindStyle,
+    VideoVolumeBarStyle volumeBarStyle,
     this.thumbnail,
     Widget loading,
     Widget buffering,
-    Widget rewind,
-    Widget forward,
-    Widget settings,
     Widget fullScreen,
     Widget fullScreenExit,
+    TextStyle textStyle,
   })  : this.loading = loading ??
             Center(
               child: CircularProgressIndicator(strokeWidth: 1.6),
@@ -26,8 +25,6 @@ class VideoViewerStyle {
                 valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
               ),
             ),
-        this.rewind = rewind ?? Icon(Icons.fast_rewind, color: Colors.white),
-        this.forward = forward ?? Icon(Icons.fast_forward, color: Colors.white),
         this.fullScreen = fullScreen ??
             Icon(
               Icons.fullscreen_outlined,
@@ -40,30 +37,27 @@ class VideoViewerStyle {
               color: Colors.white,
               size: 24,
             ),
-        this.settings = settings ??
-            Icon(
-              Icons.settings,
-              color: Colors.white,
-              size: 20,
-            ),
+        this.settingsStyle = settingsStyle ?? SettingsStyle(),
         this.progressBarStyle = progressBarStyle ?? VideoProgressBarStyle(),
         this.volumeBarStyle = volumeBarStyle ?? VideoVolumeBarStyle(),
         this.forwardAndRewindStyle =
             forwardAndRewindStyle ?? ForwardAndRewindStyle(),
-        this.playAndPauseStyle = playAndPauseStyle ?? PlayAndPauseWidgetStyle();
+        this.playAndPauseStyle = playAndPauseStyle ?? PlayAndPauseWidgetStyle(),
+        this.textStyle = textStyle ??
+            TextStyle(
+                color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold);
 
+  final SettingsStyle settingsStyle;
   final VideoVolumeBarStyle volumeBarStyle;
   final VideoProgressBarStyle progressBarStyle;
   final ForwardAndRewindStyle forwardAndRewindStyle;
   final PlayAndPauseWidgetStyle playAndPauseStyle;
   final Widget thumbnail;
   final Widget loading;
-  final Widget rewind;
-  final Widget forward;
-  final Widget settings;
   final Widget buffering;
   final Widget fullScreen;
   final Widget fullScreenExit;
+  final TextStyle textStyle;
 }
 
 class VideoProgressBarStyle {
@@ -73,16 +67,13 @@ class VideoProgressBarStyle {
     Color activeColor,
     Color bufferedColor,
     Color backgroundColor,
-    TextStyle textStyle,
     this.margin = EdgeInsets.zero,
     BorderRadiusGeometry borderRadius,
     this.paddingBeetwen = 12,
-  })  : this.activeColor = activeColor ?? Color(0xFF00b3ff),
+  })  : this.activeColor = activeColor ?? Color(0xFF295acc),
         this.bufferedColor = bufferedColor ?? Colors.white.withOpacity(0.3),
         this.backgroundColor = backgroundColor ?? Colors.white.withOpacity(0.2),
-        this.borderRadius = borderRadius ?? EdgeRadius.all(5),
-        this.textStyle =
-            textStyle ?? TextStyle(color: Colors.white, fontSize: 12);
+        this.borderRadius = borderRadius ?? EdgeRadius.all(5);
 
   final double height;
   final double paddingBeetwen;
@@ -90,48 +81,7 @@ class VideoProgressBarStyle {
   final Color activeColor;
   final Color bufferedColor;
   final Color backgroundColor;
-  final TextStyle textStyle;
   final EdgeInsetsGeometry margin;
-  final BorderRadiusGeometry borderRadius;
-}
-
-class VideoVolumeBarStyle {
-  VideoVolumeBarStyle({
-    this.width = 5,
-    this.height = 120,
-    this.color = Colors.white,
-    Color backgroundColor,
-    EdgeInsetsGeometry padding,
-    BorderRadiusGeometry borderRadius,
-    this.alignment = Alignment.centerLeft,
-  })  : this.padding = padding ?? Margin.left(20),
-        this.borderRadius = borderRadius ?? EdgeRadius.all(5),
-        this.backgroundColor = backgroundColor ?? Colors.white.withOpacity(0.2);
-
-  final double width, height;
-  final Color color;
-  final Color backgroundColor;
-  final Alignment alignment;
-  final EdgeInsetsGeometry padding;
-  final BorderRadiusGeometry borderRadius;
-}
-
-class ForwardAndRewindStyle {
-  ForwardAndRewindStyle({
-    TextStyle textStyle,
-    Color backgroundColor,
-    EdgeInsetsGeometry padding,
-    BorderRadiusGeometry borderRadius,
-  })  : this.padding = padding ?? Margin.all(5),
-        this.backgroundColor =
-            backgroundColor ?? Colors.black.withOpacity(0.25),
-        this.textStyle =
-            textStyle ?? TextStyle(color: Colors.white, fontSize: 12),
-        this.borderRadius = borderRadius ?? EdgeRadius.vertical(bottom: 5);
-
-  final TextStyle textStyle;
-  final Color backgroundColor;
-  final EdgeInsetsGeometry padding;
   final BorderRadiusGeometry borderRadius;
 }
 
@@ -142,7 +92,7 @@ class PlayAndPauseWidgetStyle {
     Color background,
     this.circleBorder,
     this.circleSize = 40,
-  })  : this.background = background ?? Color(0xFF00b3ff).withOpacity(0.8),
+  })  : this.background = background ?? Color(0xFF295acc).withOpacity(0.8),
         this.play = play ?? Icon(Icons.play_arrow, color: Colors.white),
         this.pause = pause ?? Icon(Icons.pause, color: Colors.white);
 
@@ -167,4 +117,68 @@ class PlayAndPauseWidgetStyle {
 
   Widget get playWidget => _base(play, background);
   Widget get pauseWidget => _base(pause, background);
+}
+
+class SettingsStyle {
+  SettingsStyle({
+    Widget settings,
+    Widget speed,
+    Widget selected,
+    this.paddingBetween = 32,
+  })  : this.settings = settings ??
+            Icon(
+              Icons.settings_outlined,
+              color: Colors.white,
+              size: 20,
+            ),
+        this.speed = speed ?? Icon(Icons.speed, color: Colors.white),
+        this.selected =
+            selected ?? Icon(Icons.done, color: Colors.white, size: 20);
+
+  final Widget speed;
+  final Widget selected;
+  final Widget settings;
+  final double paddingBetween;
+}
+
+class ForwardAndRewindStyle {
+  ForwardAndRewindStyle({
+    Widget rewind,
+    Widget forward,
+    Color backgroundColor,
+    EdgeInsetsGeometry padding,
+    BorderRadiusGeometry borderRadius,
+  })  : this.padding = padding ?? Margin.all(5),
+        this.backgroundColor =
+            backgroundColor ?? Colors.black.withOpacity(0.25),
+        this.borderRadius = borderRadius ?? EdgeRadius.vertical(bottom: 5),
+        this.rewind = rewind ?? Icon(Icons.fast_rewind, color: Colors.white),
+        this.forward = forward ?? Icon(Icons.fast_forward, color: Colors.white);
+
+  final Widget rewind;
+  final Widget forward;
+  final Color backgroundColor;
+  final EdgeInsetsGeometry padding;
+  final BorderRadiusGeometry borderRadius;
+}
+
+class VideoVolumeBarStyle {
+  VideoVolumeBarStyle({
+    this.width = 5,
+    this.height = 120,
+    this.color = Colors.white,
+    Color backgroundColor,
+    EdgeInsetsGeometry padding,
+    BorderRadiusGeometry borderRadius,
+    this.alignment = Alignment.centerLeft,
+  })  : this.padding = padding ?? Margin.left(20),
+        this.borderRadius = borderRadius ?? EdgeRadius.all(5),
+        this.backgroundColor = backgroundColor ?? Colors.white.withOpacity(0.2);
+
+  final double width, height;
+  final Color color;
+  final Color backgroundColor;
+  final Alignment alignment;
+  final EdgeInsetsGeometry padding;
+  final BorderRadiusGeometry borderRadius;
 }
