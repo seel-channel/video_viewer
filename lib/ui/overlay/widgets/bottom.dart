8d2aa87239ae6cd4f79bd5398d840e209e763dce
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-03-22 23:03:35 -0700

    Se agrego la opcion de los thumnails

--- lib/ui/overlay/widgets/bottom.dart
+++ lib/ui/overlay/widgets/bottom.dart
@@ -1,24 +1,60 @@
-import 'package:auto_size_text/auto_size_text.dart';
+import 'dart:typed_data';
+
 import 'package:helpers/helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 import 'package:video_viewer/data/repositories/video.dart';
+import 'package:video_viewer/domain/bloc/controller.dart';
 
 import 'package:video_viewer/ui/overlay/widgets/progress_bar.dart';
 import 'package:video_viewer/ui/overlay/widgets/background.dart';
+import 'package:video_viewer/ui/video_core/widgets/aspect_ratio.dart';
 import 'package:video_viewer/ui/widgets/play_and_pause.dart';
 import 'package:video_viewer/ui/widgets/helpers.dart';
 
-class OverlayBottomButtons extends StatefulWidget {
-  OverlayBottomButtons({Key? key}) : super(key: key);
+class OverlayBottom extends StatefulWidget {
+  OverlayBottom({Key? key}) : super(key: key);
 
   @override
-  _OverlayBottomButtonsState createState() => _OverlayBottomButtonsState();
+  _OverlayBottomState createState() => _OverlayBottomState();
 }
 
-class _OverlayBottomButtonsState extends State<OverlayBottomButtons> {
+class _OverlayBottomState extends State<OverlayBottom> {
+  ValueNotifier<Uint8List?> _image = ValueNotifier<Uint8List?>(null);
+  late VideoViewerController _controller;
   final VideoQuery _query = VideoQuery();
+
   bool _showRemaingText = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Misc.onLayoutRendered(() {
+      _controller = _query.video(context);
+      _controller.video!.addListener(_thumbnailListener);
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.video!.removeListener(_thumbnailListener);
+    super.dispose();
+  }
+
+  void _thumbnailListener() async {
+    if (_controller.isDraggingProgressBar) {
+      _image.value = await VideoThumbnail.thumbnailData(
+        video:
+            "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4",
+        imageFormat: ImageFormat.WEBP,
+        maxHeight: 64,
+        quality: 75,
+        timeMs: _controller.video!.value.position.inMilliseconds,
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +62,7 @@ class _OverlayBottomButtonsState extends State<OverlayBottomButtons> {
     final video = _query.video(context, listen: true);
 
     final isFullscreen = video.isFullScreen;
-    final controller = video.controller!;
+    final controller = video.video!;
     final barStyle = style.progressBarStyle;
     final padding = barStyle.paddingBeetwen;
 
@@ -44,6 +80,12 @@ class _OverlayBottomButtonsState extends State<OverlayBottomButtons> {
             PlayAndPause(
               type: PlayAndPauseType.bottom,
               padding: Margin.all(padding),
+            ),
+            ValueListenableBuilder(
+              valueListenable: _image,
+              builder: (_, Uint8List? bytes, __) => VideoCoreAspectRadio(
+                child: bytes != null ? Image.memory(bytes) : SizedBox(),
+              ),
             ),
             Expanded(child: VideoProgressBar()),
             SizedBox(width: padding),
