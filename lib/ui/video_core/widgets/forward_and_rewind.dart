5b1b338db4388738f505a31bdb1fb327a0c7bd81
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-03-23 16:32:31 -0700

    Al hacer el forward se muestra la barra de progreso en pequeño

--- lib/ui/video_core/widgets/forward_and_rewind.dart
+++ lib/ui/video_core/widgets/forward_and_rewind.dart
@@ -53,32 +53,78 @@ class VideoCoreForwardAndRewindLayout extends StatelessWidget {
   }
 }
 
-class VideoCoreForwardAndRewindTextAlert extends StatelessWidget {
-  const VideoCoreForwardAndRewindTextAlert({
+class VideoCoreForwardAndRewindAlert extends StatelessWidget {
+  const VideoCoreForwardAndRewindAlert({
     Key? key,
-    this.amount,
+    required this.seconds,
   }) : super(key: key);
 
-  final int? amount;
+  final int seconds;
 
   @override
   Widget build(BuildContext context) {
     final query = VideoQuery();
+    final video = query.video(context).video!;
     final style = query.videoStyle(context);
+    final progressStyle = style.progressBarStyle;
     final forwardStyle = style.forwardAndRewindStyle;
 
-    return Align(
-      alignment: Alignment.topCenter,
+    final position = video.value.position;
+    final duration = video.value.duration;
+
+    final width = 100.0;
+    final height = style.progressBarStyle.bar.height;
+
+    // return Align(
+    //   alignment: Alignment.topCenter,
+    //   child: Container(
+    //     padding: forwardStyle.padding,
+    //     decoration: BoxDecoration(
+    //       color: forwardStyle.backgroundColor,
+    //       borderRadius: forwardStyle.borderRadius,
+    //     ),
+    //     child: AutoSizeText(
+    //       query.durationFormatter(Duration(seconds: amount!)),
+    //       style: style.textStyle,
+    //     ),
+    //   ),
+    // );
+
+    return Center(
       child: Container(
         padding: forwardStyle.padding,
         decoration: BoxDecoration(
           color: forwardStyle.backgroundColor,
           borderRadius: forwardStyle.borderRadius,
         ),
-        child: AutoSizeText(
-          query.durationFormatter(Duration(seconds: amount!)),
-          style: style.textStyle,
-        ),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          AutoSizeText(
+            query.durationFormatter(Duration(seconds: seconds)),
+            style: style.textStyle,
+          ),
+          ClipRRect(
+            borderRadius: progressStyle.bar.borderRadius,
+            child: SizedBox(
+              height: height,
+              width: width,
+              child: Stack(children: [
+                Container(color: progressStyle.bar.background),
+                Container(
+                  height: height,
+                  width: ((position.inSeconds + seconds) / duration.inSeconds) *
+                      width,
+                  color: progressStyle.bar.dot,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 1,
+                  margin: Margin.left(
+                      (position.inSeconds / duration.inSeconds) * width),
+                ),
+              ]),
+            ),
+          ),
+        ]),
       ),
     );
   }
