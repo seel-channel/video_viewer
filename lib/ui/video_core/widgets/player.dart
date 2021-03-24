9ed6261019ef9e0fe2740b2a46494d721496a69b
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-03-24 11:55:21 -0700

    Se quito el Transform.scale

--- lib/ui/video_core/widgets/player.dart
+++ lib/ui/video_core/widgets/player.dart
@@ -1,9 +1,5 @@
-import 'package:helpers/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-
-import 'package:video_viewer/ui/video_core/widgets/orientation.dart';
-import 'package:video_viewer/ui/video_core/widgets/aspect_ratio.dart';
 import 'package:video_viewer/data/repositories/video.dart';
 
 class VideoCorePlayer extends StatefulWidget {
@@ -14,60 +10,14 @@ class VideoCorePlayer extends StatefulWidget {
 }
 
 class _VideoCorePlayerState extends State<VideoCorePlayer> {
-  final VideoQuery _query = VideoQuery();
-  final ValueNotifier<double> _scale = ValueNotifier<double>(1.0);
-  double _maxScale = 1.0, _minScale = 1.0, _initialScale = 1.0;
-
-  void _onScaleStart(ScaleStartDetails details) {
-    final size = context.media.size;
-    final video = _query.video(context).video!;
-    final aspectWidth = size.height * video.value.aspectRatio;
-
-    _initialScale = _scale.value;
-    _maxScale = size.width / aspectWidth;
-    setState(() {});
-  }
-
-  void _onScaleUpdate(ScaleUpdateDetails details) {
-    final double newScale = _initialScale * details.scale;
-    if (newScale >= _minScale && newScale <= _maxScale) _scale.value = newScale;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return VideoCoreOrientation(
-      builder: (isFullScreenLandscape) {
-        return Stack(children: [
-          isFullScreenLandscape
-              ? ValueListenableBuilder(
-                  valueListenable: _scale,
-                  builder: (_, double value, __) {
-                    return Transform.scale(
-                      scale: value,
-                      child: Center(
-                        child: VideoCoreAspectRadio(child: _Player()),
-                      ),
-                    );
-                  },
-                )
-              : _Player(),
-          GestureDetector(
-            onScaleStart: isFullScreenLandscape ? _onScaleStart : null,
-            onScaleUpdate: isFullScreenLandscape ? _onScaleUpdate : null,
-            child: Container(color: Colors.transparent),
-          ),
-        ]);
-      },
+    final video = VideoQuery().video(context, listen: true).video;
+    return Center(
+      child: AspectRatio(
+        aspectRatio: video!.value.aspectRatio,
+        child: VideoPlayer(video),
+      ),
     );
-  }
-}
-
-class _Player extends StatelessWidget {
-  const _Player({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final video = VideoQuery().video(context, listen: true);
-    return VideoPlayer(video.video!);
   }
 }
