4bb12a16d87a1cbe2f15fd640c52de62c4000e18
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-03-24 00:39:40 -0700

    Se agrego un CustomPainter que dibuja el Identificador de donde inicia el horizontal drag

--- lib/domain/entities/styles/bar.dart
+++ lib/domain/entities/styles/bar.dart
@@ -4,6 +4,7 @@ import 'package:helpers/helpers.dart';
 class BarStyle {
   final BorderRadius borderRadius;
   final Color secondBackground;
+  final double identifierWidth;
   final Color identifier;
   final Color background;
   final double height;
@@ -24,6 +25,7 @@ class BarStyle {
   /// The **[borderRadius]** of the border that will have the progress bar and the PreviewFrame
   BarStyle.progress({
     this.height = 5,
+    double dotSize = 5,
     Color? dot,
     Color? color,
     Color? buffered,
@@ -34,7 +36,8 @@ class BarStyle {
         this.color = color ?? Color(0xFF295acc),
         this.secondBackground = buffered ?? Colors.white.withOpacity(0.3),
         this.background = background ?? Colors.white.withOpacity(0.2),
-        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0);
+        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0),
+        this.identifierWidth = dotSize;
 
   /// The **[borderRadius]** that the VolumeBar will have
   ///
@@ -53,7 +56,8 @@ class BarStyle {
         this.color = color ?? Colors.white,
         this.secondBackground = Colors.transparent,
         this.background = background ?? Colors.white.withOpacity(0.2),
-        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0);
+        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0),
+        this.identifierWidth = 0.0;
 
   BarStyle.forward({
     this.width = 120,
@@ -61,10 +65,12 @@ class BarStyle {
     Color? color,
     Color? background,
     Color? identifier,
+    double identifierWidth = 2.0,
     BorderRadius? borderRadius,
   })  : this.identifier = identifier ?? Colors.red,
         this.color = color ?? Colors.white,
         this.secondBackground = Colors.transparent,
         this.background = background ?? Colors.white.withOpacity(0.2),
-        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0);
+        this.borderRadius = borderRadius ?? EdgeRadius.all(5.0),
+        this.identifierWidth = identifierWidth;
 }
--- lib/ui/video_core/widgets/forward_and_rewind/bar.dart
+++ lib/ui/video_core/widgets/forward_and_rewind/bar.dart
@@ -1,4 +1,3 @@
-import 'package:helpers/helpers.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:video_viewer/data/repositories/video.dart';
@@ -37,31 +36,66 @@ class VideoCoreForwardAndRewindBar extends StatelessWidget {
             style: style.textStyle,
           ),
           SizedBox(height: forwardStyle.spaceBeetweenBarAndText),
-          ClipRRect(
-            borderRadius: forwardStyle.borderRadius,
-            child: SizedBox(
-              height: height,
-              width: width,
-              child: Stack(children: [
-                Container(color: forwardStyle.bar.background),
-                Container(
-                  height: height,
-                  width: ((position.inSeconds + seconds) / duration.inSeconds) *
-                      width,
+          SizedBox(
+            height: height,
+            width: width,
+            child: Stack(children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: forwardStyle.bar.background,
+                  borderRadius: forwardStyle.borderRadius,
+                ),
+              ),
+              Container(
+                height: height,
+                width: ((position.inSeconds + seconds) / duration.inSeconds) *
+                    width,
+                decoration: BoxDecoration(
                   color: forwardStyle.bar.color,
+                  borderRadius: forwardStyle.borderRadius,
                 ),
-                Container(
+              ),
+              CustomPaint(
+                painter: _InitialPositionIdentifierPainter(
                   color: forwardStyle.bar.identifier,
-                  width: 1,
-                  margin: Margin.left(
-                    (position.inSeconds / duration.inSeconds) * width,
-                  ),
+                  position: (position.inSeconds / duration.inSeconds) * width,
+                  width: forwardStyle.bar.identifierWidth,
                 ),
-              ]),
-            ),
+                size: Size.infinite,
+              ),
+            ]),
           ),
         ]),
       ),
     );
   }
+}
+
+class _InitialPositionIdentifierPainter extends CustomPainter {
+  _InitialPositionIdentifierPainter({
+    required this.color,
+    required this.position,
+    this.width = 2.0,
+  });
+
+  final Color color;
+  final double position;
+  final double width;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()..color = color;
+    final RRect rect = RRect.fromRectAndRadius(
+      Offset(position, -size.height / 2) & Size(width, size.height * 2),
+      Radius.circular(width),
+    );
+    canvas.drawRRect(rect, paint);
+  }
+
+  @override
+  bool shouldRepaint(_InitialPositionIdentifierPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(_InitialPositionIdentifierPainter oldDelegate) =>
+      false;
 }
