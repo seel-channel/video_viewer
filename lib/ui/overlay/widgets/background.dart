e35cbc61eb0a13ac4fbd952ec5d0dd44ff9f3a29
Luis Felipe Murguia Ramos <65832922+seel-channel@users.noreply.github.com>
2021-07-27 19:08:17 -0500

    Removed Direction enum and it was replaced by begin and end axis arguments/

--- lib/ui/overlay/widgets/background.dart
+++ lib/ui/overlay/widgets/background.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:video_viewer/data/repositories/video.dart';
 
-enum Direction { bottom, top }
-
 class GradientBackground extends StatelessWidget {
   const GradientBackground({
     Key? key,
     this.child,
-    this.direction = Direction.bottom,
+    this.begin = Alignment.topCenter,
+    this.end = Alignment.bottomCenter,
   }) : super(key: key);
 
   final Widget? child;
-  final Direction direction;
+  final Alignment begin;
+  final Alignment end;
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +21,9 @@ class GradientBackground extends StatelessWidget {
       style.progressBarStyle.backgroundColor,
     ];
 
-    return Container(
+    return DecoratedBox(
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors:
-              direction == Direction.bottom ? colors : colors.reversed.toList(),
-        ),
+        gradient: LinearGradient(end: end, begin: begin, colors: colors),
       ),
       child: child,
     );
